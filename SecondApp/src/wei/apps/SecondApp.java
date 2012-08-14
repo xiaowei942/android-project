6fcd86c538b018fcd96527b25704ba8305530cef
liwei <xiaowei942@sohu.com>
2012-08-14 15:07:53 +0800

    SecondApp 完成功能首次提交

--- SecondApp/src/wei/apps/SecondApp.java
+++ SecondApp/src/wei/apps/SecondApp.java
@@ -2,14 +2,30 @@ package wei.apps;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import java.util.Date;
 
-public class SecondApp extends Activity
+public class SecondApp extends Activity implements View.OnClickListener
 {
+	Button btn;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        btn = (Button)findViewById(R.id.button);
+        btn.setOnClickListener(this);
+        updateTime();
+    }
+    
+    public void onClick(View view){
+    	updateTime();
+    }
+    private void updateTime(){
+    	btn.setText(new Date().toLocaleString());
     }
 }
